c83744208dd1b6547986f7a140f11dd1843fa67b
Cassin01 <koyojb@gmail.com>
2018-11-08 16:20:29 +0900

    add main.cpp

--- main.cpp
+++ main.cpp
@@ -16,6 +16,7 @@ Parser<int> number = [](Source *s) {
 auto number = apply<int, std::string>(toInt, many1(digit));
 
 
+/*
 Parser<int> expr = [](Source *s) {
   int x = number(s);
   auto xs = many(
@@ -23,6 +24,32 @@ Parser<int> expr = [](Source *s) {
     ||  char1('-') >> -number
   )(s);
   return std::accumulate(xs.begin(), xs.end(), x);
+};
+*/
+
+Parser<int> expr = [](Source *s) {
+  int x = number(s);
+  auto xs = many(
+    Parser<std::function<int (int)>>([](Source *s) {// 型を修正
+      char1('+')(s); 
+      int x = number(s);                            // キャプチャされる変数
+      return [=](int y) { return y + x; };          // クロージャを返す
+    }) || Parser<std::function<int (int)>>([](Source *s) {
+      char1('-')(s); 
+      int x = number(s);                            // キャプチャされる変数
+      return [=](int y) { return y - x; };          // クロージャを返す
+    }) || Parser<std::function<int (int)>>([](Source *s) { // 追加: 掛け算
+      char1('*')(s); 
+      int x = number(s);                            // キャプチャされる変数
+      return [=](int y) { return y * x; };          // クロージャを返す
+    }) || Parser<std::function<int (int)>>([](Source *s) { // 追加: 割り算
+      char1('/')(s); 
+      int x = number(s);                            // キャプチャされる変数
+      return [=](int y) { return y / x; };          // クロージャを返す
+    })
+  )(s);
+  return std::accumulate(xs.begin(), xs.end(), x,
+    [](int x, const std::function<int (int)> &f) { return f(x); });
 };
 
 
@@ -33,4 +60,7 @@ int main() {
   parseTest(expr,   "1+2+3");
   parseTest(expr,   "1-2-3");
   parseTest(expr,   "1-2+3");
+  parseTest(expr,   "2*3+4"); //
+  parseTest(expr,   "2+3*4");
+  parseTest(expr,   "100/10/2");
 }
