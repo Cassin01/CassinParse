64e47a8fed625856839cf4d7a83a94c44c3af997
Cassin01 <koyojb@gmail.com>
2018-11-08 17:14:48 +0900

    非ラムダ化

--- main.cpp
+++ main.cpp
@@ -7,38 +7,62 @@ int toInt(const std::string &s) {
   return ret;
 }
 
+Parser<std::function<int (int)>> apply( // 特殊化
+  const std::function<int (int, int)> &f, const Parser<int> &p) {
+  return apply<int, int, int>(f, p);
+}
+
 auto number = apply<int, std::string>(toInt, many1(digit));
 
+/*
 Parser<int> term = [](Source *s) {
   int x = number(s);
   auto xs = many(
     Parser<std::function<int (int)>>([](Source *s) {
       char1('*')(s);
-      int x = number(s);
-      return [=](int y) { return y * x; };
+      return apply([](int x, int y) { return y * x; }, number)(s);
     }) || Parser<std::function<int (int)>>([](Source *s) {
       char1('/')(s);
-      int x = number(s);
-      return [=](int y) { return y / x; };
+      return apply([](int x, int y) { return y / x; }, number)(s);
     })
+  )(s);
+  return std::accumulate(xs.begin(), xs.end(), x,
+    [](int x, const std::function<int (int)> &f) { return f(x); });
+};
+*/
+Parser<int> term = [](Source *s) {
+  int x = number(s);
+  auto xs = many(
+       char1('*') >> apply([](int x, int y) { return y * x; }, number)
+    || char1('/') >> apply([](int x, int y) { return y / x; }, number)
   )(s);
   return std::accumulate(xs.begin(), xs.end(), x,
     [](int x, const std::function<int (int)> &f) { return f(x); });
 };
 
 
+/*
 Parser<int> expr = [](Source *s) {
   int x = term(s);                                   // 項を取得
   auto xs = many(
     Parser<std::function<int (int)>>([](Source *s) {
       char1('+')(s);
-      int x = term(s);                               // 項を取得
-      return [=](int y) { return y + x; };
+      return apply([](int x, int y) { return y + x; }, term)(s);
     }) || Parser<std::function<int (int)>>([](Source *s) {
       char1('-')(s);
-      int x = term(s);                               // 項を取得
-      return [=](int y) { return y - x; };
+      return apply([](int x, int y) { return y - x; }, term)(s);
     })
+  )(s);
+  return std::accumulate(xs.begin(), xs.end(), x,
+    [](int x, const std::function<int (int)> &f) { return f(x); });
+};
+*/
+
+Parser<int> expr = [](Source *s) {
+  int x = term(s);
+  auto xs = many(
+       char1('+') >> apply([](int x, int y) { return y + x; }, term)
+    || char1('-') >> apply([](int x, int y) { return y - x; }, term)
   )(s);
   return std::accumulate(xs.begin(), xs.end(), x,
     [](int x, const std::function<int (int)> &f) { return f(x); });
